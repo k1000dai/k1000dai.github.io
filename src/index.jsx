5af586d4f7e932bfa5d1ca738f704426868a5509
k1000dai <chiyodakku1000@gmail.com>
2024-12-22 14:41:41 +0100

    img test

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,12 @@
-import {createRoot} from "react-dom/client";
-createRoot(document.getElementById('root'))
-    .render(<div>hello world!</div>);  
+import React from "react";
+import geminiLogo from './resources/gemini-logo.png';
+import { createRoot } from "react-dom";
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+    <div>
+        <h1>Hello, world!</h1>
+        <img src={geminiLogo} />
+    </div>
+)
